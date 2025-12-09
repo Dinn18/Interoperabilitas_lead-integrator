7200bbd1df75f2d62c02f5a2d63e9d75823c9c2b
Dinn18 <achdinnn@gmail.com>
2025-12-09 14:06:22 +0700

    Update Code

--- server.js
+++ server.js
@@ -6,25 +6,24 @@ const URL_API_MHS1 = " https://api-vendor-a-warung-legacy.vercel.app/vendora/pro
 const URL_API_MHS2 = " https://api-vendor-b-distro-modern.vercel.app/vendorb/products"; // Nuris
 const URL_API_MHS3 = " https://api-vendor-c-resto-kuliner.vercel.app/vendorc/products"; // Firman Ardiyansyah
 
-async function getDataMahasiswa1() {
+async function mahasiswa1() {
     const res = await fetch(URL_API_MHS1);
     const data = await res.json();
-    const formattedData = data.map(item => ({
+    const initialData = data.map(item => ({
         Id: item.kd_produk,
         nama: item.nm_brg,
         harga_final: parseInt(item.hrg, 10) * 0.9,
-        // harga final: item.hrg * 0.9,
         status: (item.ket_stok == "ada") ? "Tersedia" : "Tidak Tersedia",
         sumber: "Vendor A"
     }));
 
-    return formattedData;
+    return initialData;
 }
 
-async function getDataMahasiswa2() {
+async function mahasiswa2() {
     const res = await fetch(URL_API_MHS2);
     const data = await res.json();
-    const formattedData = data.map(item => ({
+    const initialData = data.map(item => ({
         id: item.sku,
         nama: item.productName, 
         harga_final: item.price,
@@ -32,29 +31,29 @@ async function getDataMahasiswa2() {
         sumber: "Vendor B"
     }));
 
-    return formattedData;
+    return initialData;
 }
 
-app.get('/all-vendors', async (req, res) => {
+app.get('/', async (req, res) => {
     try {
-        const [dataA, dataB, dataC] = await Promise.all([
-            getDataMahasiswa1(),
-            getDataMahasiswa2(),
-            getDataMahasiswa3()
+        const [data_A, data_B, data_C] = await Promise.all([
+            mahasiswa1(),
+            mahasiswa2(),
+            mahasiswa3()
         ]);
-        const allData = [...dataA, ...dataB, ...dataC];
+        const getAllData = [...data_A, ...data_B, ...data_C];
 
-        res.json({kel6: 'Project Api Banyuwangi Market Place', data: allData});
+        res.json({kel6: 'Project Api Banyuwangi Market Place', data: getAllData});
 
     } catch (error) {
         res.status(500).send('Terjadi kesalahan: ' + error.message);
     }
 });
 
-async function getDataMahasiswa3() {
+async function mahasiswa3() {
      const res = await fetch(URL_API_MHS3);
      const data = await res.json();
-     const formattedData = data.map(item => ({
+     const initialData = data.map(item => ({
          id: item.id,
          nama: `${item.details.name}${item.details.category == "Food" ? " (Recommended)" : ""}`,
          harga_final: item.pricing.base_price,
@@ -62,14 +61,14 @@ async function getDataMahasiswa3() {
          sumber: "Vendor C",
      }));
 
-     return formattedData;
+     return initialData;
 }
 
 
 app.get('/vendor-a', async (req, res) => {
     try {
-        const formattedData = await getDataMahasiswa1();
-        res.json(formattedData);
+        const initialData = await mahasiswa1();
+        res.json(initialData);
     } catch (error) {
         res.status(500).send('Terjadi kesalahan: ' + error.message);
     }
@@ -77,8 +76,8 @@ app.get('/vendor-a', async (req, res) => {
 
 app.get('/vendor-b', async (req, res) => {
     try {
-        const formattedData = await getDataMahasiswa2();
-        res.json(formattedData);
+        const initialData = await mahasiswa2();
+        res.json(initialData);
     } catch (error) {
         res.status(500).send('Terjadi kesalahan: ' + error.message);
     }
@@ -86,8 +85,8 @@ app.get('/vendor-b', async (req, res) => {
 
 app.get('/vendor-c', async (req, res) => {
     try {
-        const formattedData = await getDataMahasiswa3();
-        res.json(formattedData);
+        const initialData = await mahasiswa3();
+        res.json(initialData);
     } catch (error) {
         res.status(500).send('Terjadi kesalahan: ' + error.message);
     }
